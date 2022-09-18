142a660a1ca1e40851e8beeba9b101291cae393f
Bruno Antunes <bmvantunes@users.noreply.github.com>
2022-09-18 12:16:25 +0100

    An example how to upgrade to Jest 29

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,8 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
-  globals: {
-    'ts-jest': {
-      tsconfig: './tsconfig.jest.json',
-    },
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', { tsconfig: './tsconfig.jest.json' }],
   },
   setupFilesAfterEnv: ['./src/jest.setup.ts'],
   coverageThreshold: {
